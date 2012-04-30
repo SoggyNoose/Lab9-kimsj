a7defaa810176803654d0ac95241570497c0aeed
Sam Kim <kimsj@rose-hulman.edu>
2012-04-30 12:57:10 -0400

    Write code to pass scenario. Green as a cuke

--- ChatBot-kimsj/chatbot.rb
+++ ChatBot-kimsj/chatbot.rb
@@ -2,3 +2,9 @@ STDOUT.sync = true
 puts 'What is your name?'
 name = gets.chomp
 puts "Hello, #{name}!"
+puts "Choose one of the following games:"
+puts "1. Chess"
+puts "2. World of Warcraft"
+puts "3. Tiddlywinks"
+puts "4. Global Thermonuclear War"
+puts "5. A Punch to the Teeth"
